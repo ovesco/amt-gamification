7d8b47f3d49d8641e145875a414dfa961c53ddda
Nortalle <vincent.guidoux1@heig-vd.ch>
2018-11-16 15:02:19 +0100

    build.sh updated

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd gamification && mvn clean install
-cp C:/Users/Nortalle/Desktop/Semestre_5/amt-gamification/gamification/target/game.war C:/Users/Nortalle/Desktop/Semestre_5/amt-gamification/images/payara
+cd ..
+cp $PWD/gamification/target/game.war $PWD/images/payara
